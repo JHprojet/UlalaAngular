5ea3a87743ef5bcf88a0d0e4abbcde745de2e1d6
JHprojet <JHprojet@users.noreply.github.com>
2020-05-26 11:20:21 +0200

    Internationalization of classe component

--- src/app/classe/classe.component.ts
+++ src/app/classe/classe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClasseDAL } from '../service/classe-dal';
 import { Classe } from '../models/Classe';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-classe',
@@ -8,27 +9,38 @@ import { Classe } from '../models/Classe';
   styleUrls: ['./classe.component.css']
 })
 export class ClasseComponent implements OnInit {
+  //Variable to fill in select
   selectClasse: Classe[];
+  //Variable for active classe on select
   ClasseActive: Classe;
+  //Lang
+  Lang:string;
 
-  constructor(private classeService:ClasseDAL) { }
+  constructor(private translate:TranslateService, private classeService:ClasseDAL) { }
 
-  //Note globale : Contenu à prévoir.
+  //Global note : Need to add content (Not a priority ATM).
 
   ngOnInit(): void {
+    //Get current langage
+    this.Lang = this.translate.currentLang;
+    //Initialize variables
     this.ClasseActive = new Classe({});
     this.selectClasse = new Array<Classe>();
-    //Remplissage liste déroulante avec classes
+    //Fill the select with all classes
     this.classeService.getClasses().subscribe(response => {
       this.selectClasse = response;
     });
+    //Register to LangChangeEvent and write selected langage in variable Lang
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.Lang = event.lang;
+    });
   }
 
-  //Display de la classe choisie uniquement.
+  //Change variable for displaying only one classe information
   changeClasse(Classe)
   {
-    this.classeService.getClasse(Classe.value).subscribe(response => {
-      this.ClasseActive = response;
+    this.classeService.getClasse(Classe.value).subscribe(result => {
+      this.ClasseActive = result;
     });
   }
 }
